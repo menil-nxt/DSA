a8da1d87acbd81c6d7c941db492490097fd789be
Menil Jotaniya <menil.nxt@gmail.com>
2025-10-05 16:36:27 +0530

    Remove Duplicate in sorted Array

--- wram up/reverse_integer.js
+++ wram up/reverse_integer.js
@@ -1,22 +1,22 @@
 // Reverse integer  Ex : 231 -> 132.
 
 
-// function reverse(x){
-//     let xCopy = x;
-//     x = Math.abs(x); 
+function reverse(x){
+    let xCopy = x;
+    x = Math.abs(x); 
 
-//     let rev = 0;
-//     while (x > 0){
-//         let rem = x % 10;
-//         rev = (10 * rev) + rem;
-//         x = Math.floor(x / 10);
-//     }
+    let rev = 0;
+    while (x > 0){
+        let rem = x % 10;
+        rev = (10 * rev) + rem;
+        x = Math.floor(x / 10);
+    }
 
-//     let limit = Math.pow(2,31);                 // 2 power 331 ==> 2147483648 you can not engter value less than 2 power 31 or more than 2 power 31.
-//     if (rev < -limit || rev > limit) return 0;
+    let limit = Math.pow(2,31);                 // 2 power 331 ==> 2147483648 you can not engter value less than 2 power 31 or more than 2 power 31.
+    if (rev < -limit || rev > limit) return 0;
 
-//     return (xCopy < 0) ? -rev : rev;
-// }
+    return (xCopy < 0) ? -rev : rev;
+}
 
 // let x = 120;
 // let result = reverse(x);
@@ -30,7 +30,7 @@ var reverse = function (x) {
 
     while (num > 0) {
         const lastDigit = num % 10;
-        result = result * 10 + lastDigit; // build reversed number
+        result = result * 10 + lastDigit; // build reversed number      
         num = Math.floor(num / 10);
     }
 
