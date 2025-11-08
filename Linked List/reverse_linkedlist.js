b7a75b46c2a4eed4949f59bdd4ebb95e9d366a2b
Menil Jotaniya <menil.nxt@gmail.com>
2025-11-08 09:46:53 +0530

    reverse linked list

---
+++ Linked List/reverse_linkedlist.js
@@ -0,0 +1,12 @@
+// reverse linked list
+function reverseLinkedList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  return prev;
+}
