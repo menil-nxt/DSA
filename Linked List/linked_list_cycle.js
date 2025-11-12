25b9bb4947078e1ee401e496078e4ec68965c29f
Menil Jotaniya <menil.nxt@gmail.com>
2025-11-12 23:20:00 +0530

    Linked-list cycle Approch-2 (floyd's)

--- Linked List/linked_list_cycle.js
+++ Linked List/linked_list_cycle.js
@@ -1,6 +1,6 @@
 // Linked List Cycle
 
-// Approch - 1
+// Approch - 1    ( Hash Map OR Hash Table)
 function hasCycle(head) {
   let seenNodes = new Set(); // creating set
   let curr = head;
@@ -13,4 +13,22 @@ function hasCycle(head) {
     seenNodes.add(curr); // otherwise add inside set that new current element
   }
   return false; // if null is there than while loop is break than it's return false.
+}
+
+// Approch - 2    (Floyd's  Algorithm) OR (Floyd's Cycle Finding Algorithm)
+
+function hasCycle(head) {
+  if (!head) return false;
+
+  let slow = head;
+  let fast = head.next; // because of i don't want to fast and slow both are collepssed at first place because we are starting from their.
+
+  while (slow == fast) {
+    if (fast == null || fast.next == null) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return true;
 }
