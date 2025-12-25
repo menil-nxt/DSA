a592b77b73e16c9e618ac3592dd21140e49dc20c
Menil Jotaniya <menil.nxt@gmail.com>
2025-12-25 10:30:21 +0530

    Group Of Anagram (Hashed Key)

--- String/group_anagram.js
+++ String/group_anagram.js
@@ -1,5 +1,6 @@
 // Group Of Anagram
 
+// Approsch - 1 (Sorted keys)
 function groupAnagrams(strs) {
   let map = {};
   for (let i = 0; i < strs.length; i++) {
@@ -17,5 +18,32 @@ function groupAnagrams(strs) {
     }
   }
   // spread operator return value of map object
+  return [...Object.values(map)];
+}
+
+// Approch - 2 (Hashed Key)
+
+function groupAnagrams(strs) {
+  let map = {};
+  for (let i = 0; i < strs.length; i++) {
+    // create a key
+    let freqArr = Array(26).fill(0);
+    let s = strs[i];
+    for (let j = 0; j < s.length; j++) {
+      // each s[j] index
+      let index = s[j].charCodeAt() - "a".charCodeAt();
+      freqArr[index]++;
+    }
+    let key = "";
+    for (let k = 0; k < 26; k++) {
+      key = key + String.fromCharCode(k) + freqArr[k];
+    }
+    //fill the map
+    if (!map[key]) {
+      map[key] = [s];
+    } else {
+      map[key].push(s);
+    }
+  }
   return [...Object.values(map)];
 }
