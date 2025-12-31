a3537a12a496cd0432aabdb6a0a6e066cf401416
Menil Jotaniya <menil.nxt@gmail.com>
2025-12-31 21:51:41 +0530

    Valid Parentheses (without Map)

---
+++ Stack_Queue/valid_parentheses.js
@@ -0,0 +1,23 @@
+// Valid Parentheses (without Map)
+
+function isValid(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+      stack.push(s[i]);
+    } else {
+      let top = stack.pop();
+      // if top is empty OR top === "opning Breacket" && s[i] not equal to "closing Breacket" than return false;
+      if (
+        !top ||
+        (top === "[" && s[i] != "]") ||
+        (top === "{" && s[i] != "}") ||
+        (top === "(" && s[i] != ")")
+      ) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0; // if it's length 0 than -> true , otherwise -> false;
+}
