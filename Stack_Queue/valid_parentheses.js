9a6ec3a32d55488c4f23ba52e82a4b4e73825d20
Menil Jotaniya <menil.nxt@gmail.com>
2025-12-31 21:53:39 +0530

    Valid Parentheses (with Map)

--- Stack_Queue/valid_parentheses.js
+++ Stack_Queue/valid_parentheses.js
@@ -21,3 +21,27 @@ function isValid(s) {
   }
   return stack.length === 0; // if it's length 0 than -> true , otherwise -> false;
 }
+
+// Valid Parentheses (with Map)
+
+function isValid(s) {
+  let stack = [];
+
+  let map = {
+    "[": "]",
+    "(": ")",
+    "{": "}",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]]) {
+      stack.push(s[i]);
+    } else {
+      let top = stack.pop();
+      if (!top || s[i] != map[top]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
