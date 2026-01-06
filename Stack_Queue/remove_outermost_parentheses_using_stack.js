5021b461bb95ce598e0042d0a47dc9608c6a71af
Menil Jotaniya <menil.nxt@gmail.com>
2026-01-06 06:49:08 +0530

    Remove Outermost Parentheses (withOut Using Stack)

--- Stack_Queue/remove_outermost_parentheses_using_stack.js
+++ Stack_Queue/remove_outermost_parentheses_using_stack.js
@@ -14,3 +14,22 @@ function removeOuterParentheses(s) {
   }
   return ans;
 }
+
+// Remove Outermost Parentheses (withOut Using Stack)
+
+// in above (using Stack) in stack we are just checking level so with out stack we can also do that thing
+
+function removeOuterParentheses(s) {
+  let level = 0;
+  let ans = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      level++;
+      ans += level > 1 ? s[i] : "";
+    } else {
+      ans += level > 1 ? s[i] : "";
+      level--;
+    }
+  }
+  return ans;
+}
