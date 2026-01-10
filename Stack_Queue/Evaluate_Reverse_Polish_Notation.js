5be14f7a55eb8c05c6f716ca29b158b403280ae4
Menil Jotaniya <menil.nxt@gmail.com>
2026-01-10 10:32:30 +0530

    Evaluate Reverse Polish Notation with map -> (withOut Evaluate eval() function) (more optimized)

--- Stack_Queue/Evaluate_Reverse_Polish_Notation.js
+++ Stack_Queue/Evaluate_Reverse_Polish_Notation.js
@@ -21,6 +21,9 @@ function evalRPN(arr) {
   // convert in number -> Number("345");  OR  +("345"); -> convert to in nnumber
   return Number(stack.pop()); // +stack.pop();
 }
+
+// if (arr[i] === "+" || arr[i] === "-" || arr[i] === "*" || arr[i] === "/") -> ["+", "-", "*", "/"].includes(arr[i])
+
 function evalRPN(arr) {
   let stack = [];
   for (let i = 0; i < arr.length; i++) {
@@ -33,6 +36,35 @@ function evalRPN(arr) {
       let ans = eval(`${b} ${arr[i]} ${a}`); // ex - eval("5" + "+" + "4") -> return 9
 
       stack.push(Math.trunc(ans));
+    } else {
+      stack.push(arr[i]);
+    }
+  }
+  // return it but in number not in string
+  // convert in number -> Number("345");  OR  +("345"); -> convert to in nnumber
+  return Number(stack.pop()); // +stack.pop();
+}
+
+// with map -> (withOut Evaluate eval() function) (more optimized)
+
+function evalRPN(arr) {
+  let stack = [];
+
+  // with map you an also do same thing -> no need to write eval function
+  let map = {
+    "+": (a, b) => b + a, // every mapOfKey -> take callback function
+    "-": (a, b) => b - a,
+    "*": (a, b) => b * a,
+    "/": (a, b) => Math.trunc(b / a),
+  };
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]]) {
+      let a = stack.pop();
+      let b = stack.pop();
+
+      // with out evel() function
+      let ans = map[arr[i]](Number(a), Number(b)); // map[arr[i]] with number(a,b)
+      stack.push(ans);
     } else {
       stack.push(arr[i]);
     }
