6dbc3afebf826726e185360a8513229d70f20f59
Menil Jotaniya <menil.nxt@gmail.com>
2026-01-24 11:58:52 +0530

    Next Greter Element II(Simple Approch)

--- Stack_Queue/next_greter_element_II.js
+++ Stack_Queue/next_greter_element_II.js
@@ -23,3 +23,32 @@ function nextGreaterElements(nums) {
   }
   return ans.slice(0, n / 2);
 }
+
+function nextGreaterElements(nums) {
+  let arr = nums.concat(nums); // arr = [...nums, ...nums]; <- spread operator (same)
+  let stack = [];
+  let n = arr.length;
+
+  let ans = [];
+
+  stack.push(arr[n - 1]);
+  ans[n - 1] = -1;
+
+  for (let i = n - 2; i >= 0; i--) {
+    while (stack.length) {
+      let top = stack[stack.length - 1];
+      if (arr[i] < top) {
+        ans[i] = top;
+        break;
+      } else {
+        stack.pop();
+      }
+    }
+    if (!stack.length) {
+      ans[i] = -1;
+    }
+    stack.push(arr[i]);
+  }
+  // n -> double [nums,nums] ans n/2 [nums]
+  return ans.slice(0, n / 2); // only single array value show in ans
+}
