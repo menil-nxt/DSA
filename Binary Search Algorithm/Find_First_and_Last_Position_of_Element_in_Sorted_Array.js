aee060c862ac11fbc64d1612a1b289a7d0392ab0
Menil Jotaniya <menil.nxt@gmail.com>
2026-02-13 22:28:44 +0530

    Find First and Last Position of Element in Sorted Array(Binary SEarch Approch - 2)

--- Binary Search Algorithm/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
+++ Binary Search Algorithm/Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -53,3 +53,40 @@ function searchRange(nums, target) {
 
   return arr;
 }
+
+// Approch - 2
+
+function searchRange(nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+  let ans = [-1, -1];
+
+  while (l <= r) {
+    let mid = l + Math.floor((r - l) / 2);
+    if (nums[mid] === target) {
+      ans[0] = mid;
+      r = mid - 1; // r pointer is move mid - 1 for check if any another ending index is present or not (left side move)
+    } else if (nums[mid] < target) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  l = 0;
+  r = nums.length - 1;
+
+  while (l <= r) {
+    let mid = l + Math.floor((r - l) / 2);
+    if (nums[mid] === target) {
+      ans[1] = mid;
+      l = mid + 1; // l pointer is move mid + 1 for check if any another ending index is present or not (right side move)
+    } else if (nums[mid] < target) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return ans;
+}
