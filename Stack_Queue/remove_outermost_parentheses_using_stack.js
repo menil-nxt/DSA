d5b80ea2ae872018c164f22e1c2a3307e159a9c0
Menil Jotaniya <menil.nxt@gmail.com>
2026-02-26 11:34:12 +0530

    increment

--- Stack_Queue/remove_outermost_parentheses_using_stack.js
+++ Stack_Queue/remove_outermost_parentheses_using_stack.js
@@ -49,3 +49,19 @@ function removeOuterParentheses(s) {
   }
   return ans;
 }
+
+function removeOuterParentheses(s) {
+  let level = 0;
+  let ans = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      ++level;
+      ans += level > 1 ? s[i] : "";
+    } else {
+      ans += level > 1 ? s[i] : "";
+      --level;
+    }
+  }
+  return ans;
+}
