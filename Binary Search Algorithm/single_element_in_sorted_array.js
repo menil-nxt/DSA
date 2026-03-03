360fd70d8105f614e082e7a6d154bf1eaa164c8e
Menil Jotaniya <menil.nxt@gmail.com>
2026-03-03 11:37:29 +0530

    ingle Element in a Sorted Array Binary Search Approch

--- Binary Search Algorithm/single_element_in_sorted_array.js
+++ Binary Search Algorithm/single_element_in_sorted_array.js
@@ -1,4 +1,4 @@
-// Singlr Element in a Sorted Array
+// Single Element in a Sorted Array
 
 // Linear Search Approach (without map)(with array)
 function singleNonDuplicate(nums) {
@@ -48,6 +48,44 @@ function singleNonDuplicate(nums) {
   for (let num in map) {
     if (map[num] === 1) {
       return Number(num); // Object keys are strings, so we convert back to a number
+    }
+  }
+}
+
+// Binary search
+function singleNonDuplicate(nums) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    let m = l + (r - l) / 2;
+    if (nums[m] != nums[m + 1] && nums[m] != nums[m - 1]) {
+      return nums[m];
+    }
+
+    // where is mid of pair element is exist
+
+    // pair of mid is exist on left side
+    if (nums[m] === nums[m - 1]) {
+      // where is odd exist
+      leftCount = m - 1 - l;
+      if (leftCount % 2 === 1) {
+        // odd
+        r = m - 2; // left side odd exist
+      } else {
+        l = m + 1; // right side odd is exist
+      }
+    }
+
+    // pair of mid is exist on right side
+    if (nums[m] === nums[m + 1]) {
+      // where is odd exist
+      rightCount = r - (m + 1);
+      if (rightCount % 2 === 1) {
+        l = m + 2; // right side odd is exist
+      } else {
+        r = m - 1; // left side odd is exist
+      }
     }
   }
 }
