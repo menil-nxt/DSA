3bf52c9030c5912edcd25e694da68af50b7e4528
Menil Jotaniya <menil.nxt@gmail.com>
2026-05-19 15:09:20 +0530

    Update: Binary Tree/Symmetric_Tree.js

--- Binary Tree/Symmetric_Tree.js
+++ Binary Tree/Symmetric_Tree.js
@@ -18,3 +18,23 @@ function isSymmetric(root) {
 
   return isMirror(root.left, root.right);
 }
+
+// Iterative Approch
+function isSymmetric(root) {
+  let q = [root.left, root.right];
+
+  while (q.length) {
+    let p1 = q.shift();
+    let p2 = q.shift();
+
+    if (!p1 && !p2) continue;
+
+    if (!p1 || !p2) return false;
+
+    if (p1.val != p2.val) return false;
+
+    q.push(p1.left, p2.right);
+    q.push(p1.right, p2.left);
+  }
+  return true;
+}
