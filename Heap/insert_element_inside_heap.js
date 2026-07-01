e47526ef19a3065250e77212a1c3fdafc6e68475
Menil Jotaniya <menil.nxt@gmail.com>
2026-07-01 10:08:40 +0530

    insert , delete(extract), peek element from the heap , process of heapifyUP , heapifyDown

--- Heap/insert_element_inside_heap.js
+++ Heap/insert_element_inside_heap.js
@@ -1,15 +1,13 @@
-// Insert element inside heap
-
 // craeting MinHeap
 
 class MinHeap {
   constructor() {
     this.heap = [];
   }
-  getLeftChildIndex() {
+  getLeftChildIndex(i) {
     return 2 * i + 1;
   }
-  getRightChildIndex() {
+  getRightChildIndex(i) {
     return 2 * i + 2;
   }
   getParentIndex(i) {
@@ -18,9 +16,9 @@ class MinHeap {
   insert(val) {
     this.heap.push(val);
     let lastIndex = this.heap.length - 1;
-    this.heapfyUp(lastIndex);
+    this.heapifyUp(lastIndex);
   }
-  heapfyUp(i) {
+  heapifyUp(i) {
     while (i > 0) {
       let parentIndex = this.getParentIndex(i);
       if (this.heap[i] < this.heap[parentIndex]) {
@@ -34,6 +32,51 @@ class MinHeap {
       }
     }
   }
+
+  extract() {
+    // if heap is empty than return null
+    if (this.heap.length < 1) return null;
+
+    let min = this.heap[0];
+    let lastIndex = this.heap.length - 1;
+
+    // lastIndex of heap replaced with first index
+    [this.heap[0], this.heap[lastIndex]] = [this.heap[lastIndex], this.heap[0]];
+
+    // remove lastindex
+    this.heap.pop(); // last element of heap is removed (LIFO) Array
+
+    // heapfyDown process is start
+    this.heapifyDown(0);
+
+    return min;
+  }
+
+  heapifyDown(i) {
+    // root - leftChild - rightChild which ever is small replace with root.
+    let leftIndex = this.getLeftChildIndex(i);
+    let rightIndex = this.getRightChildIndex(i);
+    let n = this.heap.length;
+
+    let smallest = i;
+
+    if (leftIndex < n && this.heap[leftIndex] < this.heap[smallest]) {
+      smallest = leftIndex;
+    }
+    if (rightIndex < n && this.heap[rightIndex] < this.heap[smallest]) {
+      smallest = rightIndex;
+    }
+    // swap smallest with i
+    if (smallest != i) {
+      [this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]];
+      this.heapifyDown(smallest);
+    }
+  }
+
+  peek() {
+    if (!this.heap.length) return null;
+    return this.heap[0];
+  }
 }
 
 let heap = new MinHeap();
@@ -43,10 +86,16 @@ heap.insert(10);
 heap.insert(67);
 heap.insert(23);
 heap.insert(34);
+console.log("peek -", heap.peek());
 heap.insert(37);
 heap.insert(34);
 heap.insert(56);
 heap.insert(27);
 heap.insert(7);
+console.log("peek -", heap.peek());
+console.log(heap.extract());
+console.log(heap.extract());
+console.log(heap.extract());
+console.log("peek -", heap.peek());
 
 console.log(heap);
